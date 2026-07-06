e17c971084b2d3321a6d028c11a700c881630d2c
vutuyenhoangg <127908561+vutuyenhoangg@users.noreply.github.com>
2026-07-06 22:31:04 +0700

    Fix position

--- app_cloud.py
+++ app_cloud.py
@@ -4679,8 +4679,8 @@ def render_winner_flag_overlay(row):
         position: absolute;
 
         /* Desktop: vị trí vùng góc phải */
-        top: 5px;
-        right: 20px;
+        top: -20px;
+        right: 0px;
 
         z-index: 3;
 
