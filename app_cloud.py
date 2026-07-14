24ebdb90381e11335fe751125fa57b62f9ad49a6
vutuyenhoangg <127908561+vutuyenhoangg@users.noreply.github.com>
2026-07-14 14:29:50 +0700

    Update app_cloud.py

--- app_cloud.py
+++ app_cloud.py
@@ -351,7 +351,7 @@ def inject_worldcup_theme():
         }}
 
         .block-container {{
-            padding-top: 0.3rem;
+            padding-top: -5rem;
             padding-bottom: 2.4rem;
             max-width: 1440px;
         }}
