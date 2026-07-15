7a83a0fd1db048de1882863170adcb231b81bee3
vutuyenhoangg <127908561+vutuyenhoangg@users.noreply.github.com>
2026-07-15 22:24:20 +0700

    Update app_cloud.py

--- app_cloud.py
+++ app_cloud.py
@@ -587,8 +587,8 @@ def inject_worldcup_theme():
         }}
 
         .wc-hero-img {{
-            width: 150px;
-            max-height: 170px;
+            width: 200px;
+            max-height: 250px;
             object-fit: contain;
             filter: drop-shadow(
                 0 16px 32px rgba(0, 0, 0, 0.34)
