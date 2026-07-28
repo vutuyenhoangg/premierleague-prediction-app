5e4e63eb662fa1dec5847ae32c6d15d634c56a05
vutuyenhoangg <127908561+vutuyenhoangg@users.noreply.github.com>
2026-07-28 20:23:14 +0700

    Update app_cloud.py

--- app_cloud.py
+++ app_cloud.py
@@ -17415,7 +17415,7 @@ def render_epl_top_scorers_club_filter(
             height: 34px;
         
             /* Đẩy riêng logo hiện tại lên trên */
-            transform: translateY(-3px);
+            transform: translateY(-5px);
         }
 
         .epl-club-filter-option-logo img {
