9459d999027d86697a81ac13fc159889c2928f10
vutuyenhoangg <127908561+vutuyenhoangg@users.noreply.github.com>
2026-07-30 23:27:31 +0700

    Update app_cloud.py

--- app_cloud.py
+++ app_cloud.py
@@ -5160,7 +5160,7 @@ def inject_prediction_score_stepper_css():
         ):has(
             div[class*="st-key-away_score_shell_"]
         )::after {
-            top: 132px !important;
+            top: 136px !important;
         }
         
         /* Mobile */
